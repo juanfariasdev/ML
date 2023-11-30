dfa5d03a0ad309d8f051dbc8e035b9baf85f06b8
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-11-30 16:23:47 -0300

    upload project

---
+++ code/YOLO/script.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+
+# Carregar o modelo YOLO
+yolo_weights_path = "config/yolov3-spp.weights"
+yolo_config_path = "config/yolov3-spp.cfg"
+yolo_net = cv2.dnn.readNet(yolo_weights_path, yolo_config_path)
+
+# Carregar as classes
+classes_path = "config/coco.names"
+with open(classes_path, "r") as classes_file:
+    classes = [line.strip() for line in classes_file.readlines()]
+
+# Obter os nomes das camadas de saída
+output_layer_names = yolo_net.getUnconnectedOutLayersNames()
+
+# Carregar o vídeo
+video_path = "../../../assets/video1.mp4"
+video_capture = cv2.VideoCapture(video_path)
+
+while True:
+    # Capturar o próximo frame do vídeo
+    ret, frame = video_capture.read()
+    if not ret:
+        break
+
+    height, width, _ = frame.shape
+
+    # Detectar objetos usando YOLO
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    yolo_net.setInput(blob)
+    yolo_outputs = yolo_net.forward(output_layer_names)
+
+    # Informações sobre detecções
+    detected_class_ids, confidences, bounding_boxes = [], [], []
+
+    # Processar as saídas de YOLO
+    for output in yolo_outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Coordenadas do objeto detectado
+                center_x, center_y, box_width, box_height = (detection[0] * width, detection[1] * height,
+                                                            detection[2] * width, detection[3] * height)
+
+                # Coordenadas do retângulo
+                x, y = int(center_x - box_width / 2), int(center_y - box_height / 2)
+
+                bounding_boxes.append([x, y, int(box_width), int(box_height)])
+                confidences.append(float(confidence))
+                detected_class_ids.append(class_id)
+
+    # Supressão não máxima para remover detecções duplicadas
+    indexes = cv2.dnn.NMSBoxes(bounding_boxes, confidences, 0.5, 0.4)
+
+    # Desenhar caixas delimitadoras e rótulos no frame
+    for i in range(len(bounding_boxes)):
+        if i in indexes:
+            x, y, w, h = bounding_boxes[i]
+            label = str(classes[detected_class_ids[i]])
+            confidence = confidences[i]
+            color = (0, 255, 0)
+
+            # Desenhar retângulo
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+            # Adicionar rótulo com confiança
+            cv2.putText(frame, f"{label} {confidence:.2f}", (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    # Exibir o frame resultante
+    cv2.imshow("YOLO Video", frame)
+
+    # Sair quando a tecla 'q' for pressionada
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberar recursos
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ code/openCV/CSRT.py
@@ -0,0 +1,65 @@
+import cv2
+
+def initialize_tracker():
+    return cv2.TrackerCSRT_create()
+
+def initialize_video_capture(video_path):
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Erro ao abrir o vídeo")
+        exit()
+    return cap
+
+def read_first_frame(cap):
+    ret, frame = cap.read()
+    if not ret:
+        print("Erro ao ler o primeiro frame do vídeo")
+        exit()
+    return frame
+
+def select_roi(frame):
+    bbox = cv2.selectROI(frame, False)
+    return bbox
+
+def main():
+    # Inicializar o rastreador CSRT
+    tracker = initialize_tracker()
+
+    # Inicializar o vídeo
+    video_path = "../../assets/video3.mp4"
+    cap = initialize_video_capture(video_path)
+
+    # Ler o primeiro frame
+    frame = read_first_frame(cap)
+
+    # Selecionar a região de interesse (ROI) para rastrear
+    bbox = select_roi(frame)
+    tracker.init(frame, bbox)
+
+    while True:
+        # Ler o próximo frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Atualizar o rastreador com o novo frame
+        success, bbox = tracker.update(frame)
+
+        # Desenhar a bounding box no frame
+        if success:
+            (x, y, w, h) = map(int, bbox)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Exibir o frame resultante
+        cv2.imshow("Tracking", frame)
+
+        # Sair quando a tecla 'q' for pressionada
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Liberar recursos
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
